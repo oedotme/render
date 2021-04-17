299dd41da67adb6f04ff314ea5302c4e5265c22a
oedotme <oedotme@gmail.com>
2021-04-17 17:37:27 +0200

    feat: update pages styles

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,3 @@
 export default function Dashboard(): JSX.Element {
-  return <h1>Dashboard</h1>
+  return <h1 className="text-2xl">This is the dashboard page</h1>
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,3 @@
 export default function Home(): JSX.Element {
-  return <h1>Home</h1>
+  return <h1 className="text-2xl">This is the home page</h1>
 }
