1189d07191a646af0e4ff431da7a2936cffde80d
oedotme <oedotme@gmail.com>
2021-05-01 16:27:13 +0200

    feat: add auth context

---
+++ src/context/auth.tsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useMemo, useState } from 'react'
+
+import { storage } from '@/utils'
+
+type User = { email: string }
+
+type AuthContext = {
+  token: string
+  user?: User
+  login: (email: string) => void
+  logout: () => void
+}
+
+const AuthContext = createContext<AuthContext | null>(null)
+
+export function AuthProvider({ children }: { children: React.ReactNode }): JSX.Element | null {
+  const [token, setToken] = useState<string>(() => storage.get('token') || '')
+  const [user, setUser] = useState<AuthContext['user']>(() => storage.get('user') || undefined)
+
+  const value = useMemo(
+    () => ({
+      token,
+      user,
+      login: (email: string) => {
+        setToken('token'), storage.set('token', 'a0731ae631bc01dea99f13b3f8ed48fc')
+        setUser({ email }), storage.set('user', { email })
+      },
+      logout: () => {
+        setToken(''), storage.remove('token')
+        setUser(undefined), storage.remove('user')
+      },
+    }),
+    [token, user]
+  )
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+export function useAuth(): AuthContext {
+  const context = useContext(AuthContext)
+  if (!context) throw Error('useAuth should be used within <AuthProvider />')
+  return context
+}
