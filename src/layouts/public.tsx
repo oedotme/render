0011889868adaed6c7f302b16adfb4f13d6f9a75
oedotme <oedotme@gmail.com>
2021-05-01 13:13:36 +0200

    feat: add logo

--- src/layouts/public.tsx
+++ src/layouts/public.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 
+import { Logo } from '@/assets'
+
 type Props = {
   children: JSX.Element
 }
@@ -9,6 +11,11 @@ export default function Guest({ children }: Props): JSX.Element {
     <>
       <header className="p-6">
         <ul>
+          <li>
+            <Link to="/">
+              <img src={Logo} alt="Render" />
+            </Link>
+          </li>
           <li>
             <Link to="/">Home</Link>
           </li>
