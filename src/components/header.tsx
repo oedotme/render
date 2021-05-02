05836af39f4366d77e3e52e18e1a7b1d21f3655b
oedotme <oedotme@gmail.com>
2021-05-02 15:59:47 +0200

    feat: update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,36 @@
 import { Link } from 'react-router-dom'
 
 import { Logo } from '@/assets'
+import { useAuth } from '@/context'
 
 export default function Header(): JSX.Element {
+  const auth = useAuth()
+
   return (
-    <header className="flex justify-between">
-      <ul className="flex gap-6 items-center">
+    <header className="flex items-center justify-between">
+      <ul className="flex gap-4 items-center font-mono">
         <li>
           <Link to="/">
             <img className="w-8 h-8 bg-gray-50 rounded-sm" src={Logo} alt="Render" />
           </Link>
         </li>
         <li>
-          <Link to="/">Home</Link>
+          <Link className="p-2 hover:underline" to="/">
+            /
+          </Link>
+        </li>
+        <li>
+          <Link className="p-2 hover:underline" to="/routes">
+            /routes
+          </Link>
         </li>
       </ul>
-      <Link to="profile">profile</Link>
+
+      {auth.token && (
+        <span className="opacity-50" role="img" aria-label="private">
+          🔒
+        </span>
+      )}
     </header>
   )
 }
