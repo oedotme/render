815c0c4287942466662c1157b00beb99d3db02a5
oedotme <oedotme@gmail.com>
2021-05-08 04:51:43 +0200

    feat: move type checking to lint staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': 'eslint --fix',
+  '*.{ts,tsx}': [() => 'tsc -p tsconfig.json', 'eslint --fix'],
+  '*.{js,jsx}': 'eslint --fix',
   '*.{css,html,json,md,mdx}': 'prettier --write',
 }
