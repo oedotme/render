de3c2a721b5a5dcf9348a6a718ac429a88252c62
oedotme <oedotme@gmail.com>
2021-05-22 22:01:01 +0200

    feat: update header links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 
-import { Logo } from '@/assets'
 import { useAuth } from '@/context'
 
 export default function Header(): JSX.Element {
@@ -10,8 +9,8 @@ export default function Header(): JSX.Element {
     <header className="flex items-center justify-between">
       <ul className="flex gap-4 items-center font-mono">
         <li>
-          <Link to="/">
-            <img className="w-8 h-8 bg-gray-50 rounded-sm" src={Logo} alt="Render" />
+          <Link className="p-2 hover:underline" to="/">
+            /home
           </Link>
         </li>
 
