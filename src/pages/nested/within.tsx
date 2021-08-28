829349dc177bc40168e2415bfc0d3e948c720b85
oedotme <oedotme@gmail.com>
2021-08-28 10:58:06 +0200

    feat: add nested routes example

---
+++ src/pages/nested/within.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+export default function NestedWithin(): JSX.Element {
+  return (
+    <>
+      <h1 className="font-mono text-2xl">/nested/within</h1>
+      <p className="mt-4 text-lg">here</p>
+      <Link className="mt-4 p-2 hover:underline" to="/routing">
+        back
+      </Link>
+    </>
+  )
+}
--- src/pages/routing.tsx
+++ src/pages/routing.tsx
@@ -5,7 +5,11 @@ export default function Routing(): JSX.Element {
     <>
       <h1 className="font-mono text-2xl">/routing</h1>
 
-      <Link className="p-2 mt-4 hover:underline" to={`/dynamic/${Date.now()}`}>
+      <Link className="p-2 mt-4 hover:underline" to="/nested/within">
+        nested route
+      </Link>
+
+      <Link className="p-2 hover:underline" to={`/dynamic/${Date.now()}`}>
         dynamic route
       </Link>
 
