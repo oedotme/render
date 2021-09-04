b3304d8d20de10a391bf0e2a25d88d0b756c0297
oedotme <oedotme@gmail.com>
2021-09-04 10:07:23 +0200

    feat: update dynamic and catch heading url

--- src/pages/catch/[...all].tsx
+++ src/pages/catch/[...all].tsx
@@ -5,7 +5,7 @@ export default function CatchAll(): JSX.Element {
 
   return (
     <>
-      <h1 className="font-mono text-2xl">/catch/[...all]</h1>
+      <h1 className="font-mono text-2xl">/catch/*</h1>
       <p className="mt-4 text-lg">{all}</p>
       <Link className="mt-4 p-2 hover:underline" to="/routing">
         ⟵
--- src/pages/dynamic/[timestamp].tsx
+++ src/pages/dynamic/[timestamp].tsx
@@ -5,7 +5,7 @@ export default function DynamicTimestamp(): JSX.Element {
 
   return (
     <>
-      <h1 className="font-mono text-2xl">/dynamic/[timestamp]</h1>
+      <h1 className="font-mono text-2xl">/dynamic/:timestamp</h1>
       <p className="mt-4 text-lg">{timestamp}</p>
       <Link className="mt-4 p-2 hover:underline" to="/routing">
         ⟵
