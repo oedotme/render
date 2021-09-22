a41afd3a4402363f564625adb0b2217e98a256f2
oedotme <oedotme@gmail.com>
2021-09-22 22:38:27 +0200

    feat: use new react plugin for vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react()],
   resolve: { alias: { '@': '/src' } },
-  esbuild: { jsxInject: `import React from 'react'` },
 })
