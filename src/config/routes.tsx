4cca2809788077a934601d03efb47d832d7d37df
oedotme <oedotme@gmail.com>
2021-11-06 22:05:04 +0200

    feat: use dynamic imports with routes

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,27 +1,25 @@
-import { Fragment } from 'react'
+import { Fragment, lazy, Suspense } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import { Guard } from '@/components'
+import { Guard } from '@/config'
 
 const PRESERVED = import.meta.globEager('/src/pages/(_app|404).tsx')
-const ROUTES = import.meta.globEager('/src/pages/**/[a-z[]*.tsx')
+const ROUTES = import.meta.glob('/src/pages/**/[a-z[]*.tsx')
 
 const preserved: Partial<Record<'_app' | '404', Component>> = Object.keys(PRESERVED).reduce((preserved, file) => {
   const key = file.replace(/\/src\/pages\/|\.tsx$/g, '')
   return { ...preserved, [key]: PRESERVED[file].default as Component }
 }, {})
 
-const routes = Object.keys(ROUTES).map((route) => {
+type LazyComponent = () => Promise<{ default: () => JSX.Element }>
+
+export const routes = Object.keys(ROUTES).map((route) => {
   const path = route
     .replace(/\/src\/pages|index|\.tsx$/g, '')
     .replace(/\[\.{3}.+\]/, '*')
     .replace(/\[(.+)\]/, ':$1')
 
-  return {
-    path,
-    component: ROUTES[route].default as Component,
-    scope: ROUTES[route]?.meta?.scope as 'private' | 'public',
-  }
+  return { path, component: lazy(ROUTES[route] as LazyComponent) }
 })
 
 export const Routes = (): JSX.Element => {
@@ -30,16 +28,19 @@ export const Routes = (): JSX.Element => {
 
   return (
     <App>
-      <Switch>
-        {routes.map(({ path, component: Component = Fragment, scope }) => (
-          <Route key={path} path={path} exact={true}>
-            <Guard scope={scope}>
-              <Component />
-            </Guard>
-          </Route>
-        ))}
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <Suspense fallback={'Loading...'}>
+        <Switch>
+          {routes.map(({ path, component: Component = Fragment }) => (
+            <Route key={path} path={path} exact={true}>
+              <Guard path={path}>
+                <Component />
+              </Guard>
+            </Route>
+          ))}
+
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Suspense>
     </App>
   )
 }
