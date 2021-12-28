e7bebc54ecba612d83e6ad6aa8698baa6a162183
oedotme <oedotme@gmail.com>
2021-12-28 22:59:55 +0200

    feat: update repo api

--- src/api/repo.ts
+++ src/api/repo.ts
@@ -1,12 +1,17 @@
 import { Query, useQuery } from '@/hooks'
 
 const url = 'https://api.github.com/repos/oedotme'
-const http = {
-  get: (url: string) => fetch(url).then((response) => response.json()),
+const get = (url: string) => fetch(url).then((response) => response.json())
+
+export type Repo = {
+  id: string
+  name: string
+  description: string
+  html_url: string
 }
 
-type Repo = { id: string; description: string }
+export const getRepo = (name = 'render'): Promise<Repo> => get(`${url}/${name}`)
 
 export const useRepo = (name = 'render'): Query<Repo> => {
-  return useQuery<Repo>(['repo', name], () => http.get(`${url}/${name}`), { cacheTime: Infinity })
+  return useQuery<Repo>(['repo', name], () => getRepo(name), { cacheTime: Infinity })
 }
