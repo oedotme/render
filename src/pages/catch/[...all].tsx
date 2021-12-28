f9bab94538197ca86c11ae2dde37735ae6152994
oedotme <oedotme@gmail.com>
2021-12-28 23:00:20 +0200

    feat: update params examples

--- src/pages/catch/[...all].tsx
+++ src/pages/catch/[...all].tsx
@@ -1,14 +1,16 @@
-import { useParams } from 'react-router-dom'
+import { MakeGenerics, useMatch } from 'react-location'
 
 import { Link } from '@/components'
 
-export default function CatchAll(): JSX.Element {
-  const { '*': all } = useParams()
+type Route = MakeGenerics<{ Params: { '*': string } }>
+
+export default function CatchAll() {
+  const { params } = useMatch<Route>()
 
   return (
     <>
       <h1 className="font-mono text-2xl">/catch/*</h1>
-      <p className="mt-4 text-lg">{all}</p>
+      <p className="mt-4 text-lg">{params['*']}</p>
       <Link className="mt-4 p-2 hover:underline" to="/routing">
         ⟵
       </Link>
--- src/pages/dynamic/[timestamp].tsx
+++ src/pages/dynamic/[timestamp].tsx
@@ -1,14 +1,16 @@
-import { useParams } from 'react-router-dom'
+import { MakeGenerics, useMatch } from 'react-location'
 
 import { Link } from '@/components'
 
-export default function DynamicTimestamp(): JSX.Element {
-  const { timestamp } = useParams()
+type Page = MakeGenerics<{ Params: { timestamp: string } }>
+
+export default function DynamicTimestamp() {
+  const { params } = useMatch<Page>()
 
   return (
     <>
       <h1 className="font-mono text-2xl">/dynamic/:timestamp</h1>
-      <p className="mt-4 text-lg">{timestamp}</p>
+      <p className="mt-4 text-lg">{params.timestamp}</p>
       <Link className="mt-4 p-2 hover:underline" to="/routing">
         ⟵
       </Link>
