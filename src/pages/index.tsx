a0e523d2f8ee64cadc672995b6ef6dc0c4109359
oedotme <oedotme@gmail.com>
2021-12-28 22:59:19 +0200

    feat: add data loaders examples

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
-import { useRepo } from '@/api'
+import { LoaderFn, MakeGenerics, useMatch } from 'react-location'
 
-export default function Home(): JSX.Element {
-  const { loading, data } = useRepo('render')
+import { Repo, getRepo } from '@/api'
+
+type Route = MakeGenerics<{ LoaderData: Repo }>
+
+export const loader: LoaderFn<Route> = () => {
+  return getRepo('render')
+}
+
+export default function Home() {
+  const { data } = useMatch<Route>()
 
   return (
     <>
-      <img className="w-32 h-32" src="/assets/icons/logo.svg" alt="render" title="render" />
-      <em className="mt-4 text-gray-700">{loading ? 'Loading...' : data ? data.description : '-'}</em>
+      <img className="w-32 h-32" src="/assets/icons/logo.svg" alt={data.name} title={data.name} />
+      <em className="mt-4 text-gray-700">{data.description}</em>
 
       <ul className="mt-8">
         <li>
-          <a href="https://github.com/oedotme/render" target="_blank" rel="noopener noreferrer">
+          <a href={data.html_url} target="_blank" rel="noopener noreferrer">
             <img className="w-6 h-6 opacity-50 hover:opacity-80" src="/assets/icons/github.svg" alt="GitHub" />
           </a>
         </li>
