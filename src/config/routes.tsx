378b6d1af4890562a2140617f8c107257a160a47
oedotme <oedotme@gmail.com>
2022-01-10 09:55:37 +0200

    feat: convert routes to a tree for nested layouts

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -9,19 +9,55 @@ const preservedRoutes: Partial<Record<string, () => JSX.Element>> = Object.keys(
   return { ...routes, [path]: PRESERVED[key]?.default }
 }, {})
 
-const regularRoutes: Route[] = Object.keys(ROUTES).map((key) => {
-  const route = ROUTES[key]
-  const path = key
-    .replace(/\/src\/pages|index|\.tsx$/g, '')
+const regularRoutes = Object.keys(ROUTES).reduce<Route[]>((routes, key) => {
+  const module = ROUTES[key]
+  const route: Route = {
+    element: () => module().then((mod) => (mod?.default ? <mod.default /> : <></>)),
+    loader: (...args) => module().then((mod) => mod?.loader?.(...args)),
+  }
+
+  const segments = key
+    .replace(/\/src\/pages|\.tsx$/g, '')
     .replace(/\[\.{3}.+\]/, '*')
     .replace(/\[(.+)\]/, ':$1')
+    .split('/')
+    .filter(Boolean)
 
-  return {
-    path,
-    element: () => route().then((mod) => (mod?.default ? <mod.default /> : <Fragment />)),
-    loader: (...args) => route().then((mod) => mod?.loader?.(...args)),
-  }
-})
+  segments.reduce((parent, segment, index) => {
+    const path = segment.replace(/index|\./g, '/')
+    const root = index === 0
+    const leaf = index === segments.length - 1 && segments.length > 1
+    const node = !root && !leaf
+    const insert = /^\w|\//.test(path) ? 'unshift' : 'push'
+
+    if (root) {
+      const ignored = path.startsWith(':') || path === '*'
+      if (ignored) return parent
+
+      const last = segments.length === 1
+      if (last) {
+        routes[insert]({ path, ...route })
+        return parent
+      }
+    }
+
+    if (root || node) {
+      const current = root ? routes : parent.children
+      const found = current?.find((route) => route.path === path)
+      if (found) found.children ??= []
+      else current?.[insert]({ path, children: [] })
+      return found || (current?.[insert === 'unshift' ? 0 : current.length - 1] as Route)
+    }
+
+    if (leaf) {
+      parent?.children?.[insert]({ path, ...route })
+    }
+
+    return parent
+  }, {} as Route)
+
+  return routes
+}, [])
 
 const App = preservedRoutes?.['_app'] || Fragment
 const NotFound = preservedRoutes?.['404'] || Fragment
