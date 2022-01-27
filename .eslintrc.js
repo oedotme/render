8a582a4b11be000731d913a6a25e26511a6021ed
oedotme <oedotme@gmail.com>
2022-01-27 20:38:36 +0200

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,8 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: { jsx: true },
-  },
-  settings: {
-    react: { version: 'detect' },
-  },
+  parserOptions: { ecmaFeatures: { jsx: true } },
+  settings: { react: { version: 'detect' } },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
