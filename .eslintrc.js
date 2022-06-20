6720e4b7ad973b9e0e313b4546838502b9f74bc3
oedotme <oedotme@gmail.com>
2022-06-20 20:55:18 +0200

    chore: add eslint simple import sort plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,22 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
   rules: {
+    'react/prop-types': 'off',
+    'simple-import-sort/exports': 'warn',
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          ['^\\u0000'],
+          ['^(assert|constants|crypto|events|fs|path|querystring|stream|url)(/.*|$)'],
+          ['^(react|solid)', '^@?\\w'],
+          ['^(@)(/.*|$)'],
+          ['^\\.'],
+        ],
+      },
+    ],
   },
   ignorePatterns: ['*rc.js', '*.config.js', 'prerender.ts'],
 }
