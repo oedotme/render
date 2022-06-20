a7a7172f78383370ecbd20e4f59f20f5386d6b25
oedotme <oedotme@gmail.com>
2022-06-20 21:04:45 +0200

    chore: use split vendor chunk vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin as vendor } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vendor()],
   resolve: { alias: { '@': '/src' } },
 })
