c29fb3e9fba1420bcd5866389ebc653f1dbf8a46
oedotme <oedotme@gmail.com>
2023-02-14 18:46:29 +0100

    feat: use json parse only with storage object values

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -2,6 +2,8 @@ const store = typeof window !== 'undefined' ? localStorage : undefined
 
 export const storage = {
   set: <Item>(key: string, value: Item): void => store?.setItem(key, JSON.stringify(value)),
-  get: <Item>(key: string): Item => JSON.parse(store?.getItem(key) || '""'),
+  get: <Item>(key: string): Item => {
+    return store?.getItem(key)?.startsWith('{') ? JSON.parse(store?.getItem(key) || '""') : store?.getItem(key)
+  },
   remove: (key: string): void => store?.removeItem(key),
 }
