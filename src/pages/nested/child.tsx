a09b5f6354097e3d467b5512335f633dbdd55c59
oedotme <oedotme@gmail.com>
2026-03-15 16:04:22 +0100

    feat: use _layout convention for nested layout

--- src/pages/nested/child.tsx
+++ src/pages/nested/child.tsx
@@ -4,7 +4,7 @@ export default function Child() {
   return (
     <>
       <h1 className="font-mono text-2xl">/nested/child</h1>
-      <p className="mt-4 text-lg">Using layout from `src/pages/nested.tsx`</p>
+      <p className="mt-4 text-lg">Using layout from `src/pages/nested/_layout.tsx`</p>
       <Link className="mt-4 p-2 hover:underline" to="/routing">
         ⟵
       </Link>
